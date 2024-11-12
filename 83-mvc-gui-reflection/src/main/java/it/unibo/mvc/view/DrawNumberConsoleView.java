e5e1c4adf1c3259d41b5e5c3d3e449689c2158e1
Matteo Todeschi - matteo.todeschi2@studio.unibo.it <matteo.todeschi2@studio.unibo.it>
2024-11-12 16:26:58 +0100

    Committed Exercise 83 -> Multiple views

---
+++ 83-mvc-gui-reflection/src/main/java/it/unibo/mvc/view/DrawNumberConsoleView.java
@@ -0,0 +1,32 @@
+package it.unibo.mvc.view;
+
+import it.unibo.mvc.api.DrawNumberController;
+import it.unibo.mvc.api.DrawNumberView;
+import it.unibo.mvc.api.DrawResult;
+
+public class DrawNumberConsoleView implements DrawNumberView {
+
+    private static final String NEW_GAME = ": a new game starts!";
+
+    @Override
+    public void setController(DrawNumberController observer) {
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Console view started...");
+    }
+
+    @Override
+    public void result(DrawResult res) {
+        switch (res) {
+            case YOURS_HIGH, YOURS_LOW -> {
+                System.out.println(res.getDescription());
+                return;
+            }
+            case YOU_WON -> System.out.println(res.getDescription() + NEW_GAME);
+            case YOU_LOST -> System.out.println(res.getDescription() + NEW_GAME);
+            default -> throw new IllegalStateException("Unknown game state");
+        }
+    }
+}
